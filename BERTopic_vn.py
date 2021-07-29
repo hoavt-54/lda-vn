9cb014b7503d3523d644e971f612837499e8d172
Hoa Vu <hoavutrongvn@gmail.com>
2021-07-29 14:46:06 +0700

    add git topics

--- BERTopic_vn.py
+++ BERTopic_vn.py
@@ -1,18 +1,22 @@
-import utils
+from vncorenlp import VnCoreNLP
 from bertopic import BERTopic
 from flair.embeddings import TransformerDocumentEmbeddings
-from sklearn.datasets import fetch_20newsgroups
-docs = fetch_20newsgroups(subset='all',  remove=('headers', 'footers', 'quotes'))['data']
+from utils import read_dataset, Tokenizer
 
 
+annotator = VnCoreNLP('VnCoreNLP/VnCoreNLP-1.1.1.jar', annotators="wseg,pos", max_heap_size='-Xmx2g')
+tk = Tokenizer(annotator)
+
+data = read_dataset()
+docs = data["text"].tolist()
+docs = [" ".join(tk.preprocess(doc, filter=False)) for doc in docs]
+
+phobert = TransformerDocumentEmbeddings('vinai/phobert-base')
+topic_model = BERTopic(embedding_model=phobert)
+topics, _ = topic_model.fit_transform(docs)
+
+topic_model.get_topic_info()
+topic_model.save("saved_models/vietnamese_vnexpress_2.model")
 
 
-
-
-data = utils.read_dataset()
-docs = data["text"].tolist
-
-
-
-roberta = TransformerDocumentEmbeddings('vinai/phobert-base')
-topic_model = BERTopic(embedding_model=roberta)
+## inference
