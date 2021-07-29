87202092602e35a473291440cd05043ff9b5e9f8
Hoa Vu <hoavutrongvn@gmail.com>
2021-07-29 09:36:40 +0700

    added stopwords

--- BERTopic_vn.py
+++ BERTopic_vn.py
@@ -1,5 +1,5 @@
 import utils
-from BERTopic.bertopic import BERTopic
+from bertopic import BERTopic
 from flair.embeddings import TransformerDocumentEmbeddings
 from sklearn.datasets import fetch_20newsgroups
 docs = fetch_20newsgroups(subset='all',  remove=('headers', 'footers', 'quotes'))['data']
@@ -10,7 +10,8 @@ docs = fetch_20newsgroups(subset='all',  remove=('headers', 'footers', 'quotes')
 
 
 data = utils.read_dataset()
-docs = data["text"].tolist()
+docs = data["text"].tolist
+
 
 
 roberta = TransformerDocumentEmbeddings('vinai/phobert-base')
